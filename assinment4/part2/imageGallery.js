8ea618ee030e1bb606f9c691a05ce9a6da4a3f4b
xandersap2021-arch <xandersap2021@gmail.com>
2026-04-10 22:03:30 -0400

    added loop and base URL

--- assinment4/part2/imageGallery.js
+++ assinment4/part2/imageGallery.js
@@ -11,3 +11,27 @@ const images = [
   { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
   { filename: "pic5.jpg", alt: "Large moth on a leaf" },
 ];
+
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+  for (const image of images) {
+  // Create a new image element
+  const newImage = document.createElement("img");
+  // Set the source and alt text for the image
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  // Make the image focusable via the keyboard
+  newImage.tabIndex = "0";
+  // Append the image as a child of the thumbBar
+  thumbBar.appendChild(newImage);
+  // Update the display to show the image full size when a thumb is clicked
+  newImage.addEventListener("click", updateDisplayedImage);
+  // Update the display to show the image full size when the "Enter" key
+  // is pressed after it has been focused
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
