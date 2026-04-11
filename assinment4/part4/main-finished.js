9aa3bce6c6f6efd2a43b339ed3215f8836c3ef33
xandersap2021-arch <xandersap2021@gmail.com>
2026-04-10 23:27:16 -0400

    added new class with constructor

--- assinment4/part4/main-finished.js
+++ assinment4/part4/main-finished.js
@@ -89,6 +89,34 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+class evilCircle extends Shape {
+
+  constructor(x, y) {
+    super(x, y, 20, 20);
+
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+}
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
