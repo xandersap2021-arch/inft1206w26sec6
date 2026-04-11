4ed39ab6fe72b5eb7513725624e17b63cd64d922
xandersap2021-arch <xandersap2021@gmail.com>
2026-04-10 21:58:55 -0400

    added data object

--- assinment4/part2/imageGallery.js
+++ assinment4/part2/imageGallery.js
@@ -3,3 +3,11 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
