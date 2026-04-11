c33db05c9a679068f9cd27081d707e5dc2f20df5
xandersap2021-arch <xandersap2021@gmail.com>
2026-04-10 20:56:12 -0400

    added java file and its raw text strings

---
+++ assinment4/part1/story.js
@@ -0,0 +1,44 @@
+// Complete variable definitions and random functions
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// Raw text strings
+
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"]
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"]
+
+
+// Partial return random string function
+
+function returnRandomStoryString() {
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  if (customName.value !== "") {
+    const name = customName.value;
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
+  }
+
+  // TODO: replace "" with the correct expression
+  story.textContent = "";
+  story.style.visibility = "visible";
+}
