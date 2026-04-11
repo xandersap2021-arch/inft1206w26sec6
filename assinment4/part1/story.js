6143a7f87c8de7622b8708bac7ddf5d8b805241d
xandersap2021-arch <xandersap2021@gmail.com>
2026-04-10 21:24:27 -0400

    final touches
    added and edited the generateStory function

--- assinment4/part1/story.js
+++ assinment4/part1/story.js
@@ -36,16 +36,22 @@ const randomCharacter = randomValueFromArray(characters);
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString();
+
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
+
 }
